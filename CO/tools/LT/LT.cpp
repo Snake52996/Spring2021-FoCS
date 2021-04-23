776c2a5dff735e96646134f28c4209650b604d6c
snake52996 <33573135+Snake52996@users.noreply.github.com>
2021-04-23 22:45:38 +0800

    enhanced interface

--- CO/tools/LT/LT.cpp
+++ CO/tools/LT/LT.cpp
@@ -12,8 +12,22 @@
 #include<string>
 #include<regex>
 #ifdef __linux__
+#define REDCOLOR "\e[31;1m"
+#define GREENCOLOR "\e[32;1m"
+#define YELLOWCOLOR "\e[33;1m"
+#define BLUECOLOR "\e[34;1m"
+#define PURPLECOLOR "\e[35;1m"
+#define AOCOLOR "\e[36;1m"
+#define ENDCOLOR "\e[0m"
 #define LOGISIM_COMMAND "timeout %d %s -tty table %s > %s 2>/dev/null",config.timeout_,config.logisim_.c_str(),circ_path.c_str(),temp_out_path.c_str()
 #else
+#define REDCOLOR ""
+#define GREENCOLOR ""
+#define YELLOWCOLOR ""
+#define BLUECOLOR ""
+#define PURPLECOLOR ""
+#define AOCOLOR ""
+#define ENDCOLOR ""
 #define LOGISIM_COMMAND "Start-Job { %s -tty table %s > %s 2>/dev/null } | Wait-Job -Timeout %d ",config.logisim_.c_str(),circ_path.c_str(),temp_out_path.c_str(),config.timeout_
 #endif
 using namespace tinyxml2;
@@ -142,17 +156,55 @@ int getConfiguration(int argc, char** argv, Configuration& config){
 }
 int verifyConfiguration(Configuration& config){
     if(!fs::is_regular_file(config.circuit_)){
+        printf("错误: 无法访问的设计文件\n");
         return ERROR_INVALID_OPTION;
     }
     if(!fs::is_directory(config.test_dir_)){
+        printf("错误: 指定的测试文件目录无法识别\n");
         return ERROR_INVALID_OPTION;
     }
     if(config.timeout_ <= 0){
+        printf("错误: 不合法的超时时间\n");
         return ERROR_INVALID_OPTION;
     }
     return ERROR_OK;
 }
 void showHelp(){
+    printf(
+        "LT: 用于Logisim的自动评测机\n"
+        "用法: LT [选项(们)]\n"
+        "选项:\n"
+        "\t--help          显示此消息并退出\n\n"
+        "\t--with-mars     指定用于调用Mars的指令\n"
+        "\t                请注意使用修改后的Mars来提供需要的信息\n"
+        "\t                不提供该选项时的默认值: ./Mars.jar\n\n"
+        "\t--test-dir      指定存放测试程序的目录\n"
+        "\t                注意LT将不会尝试递归的遍历此目录\n"
+        "\t                不提供该选项时的默认值: test\n\n"
+        "\t--thread        指定使用的线程数\n"
+        "\t                当不指定参数时，采用自动推测的数目\n"
+        "\t                不提供该选项时的默认值: 1\n\n"
+        "\t--with-logisim  指定用于调用Logisim的指令\n"
+        "\t                不提供该选项时的默认值: logisim\n\n"
+        "\t--with-circuit  指定您设计的CPU文件\n"
+        "\t                不提供该选项时的默认值: CPU.circ\n\n"
+        "\t--timeout       指定Logisim仿真的超时时间(单位: 秒)\n"
+        "\t                仿真将不会运行超过此时长\n"
+        "\t                不提供该选项时的默认值: 5\n\n"
+        "\n"
+        "如何工作:\n"
+        "\t通常而言您仅需要按照上述给出的说明正确指定参数即可使用LT\n"
+        "\t无需对您的 CPU 设计作出任何修改\n\n"
+        "\t为了避免 PC 溢出导致重复输出带来的误判\n"
+        "\t一条跳转到自身的指令将会被自动插入到汇编后的程序末尾\n"
+        "\t其编码为 0x1000ffff\n\n"
+        "\t通常仿真将在超时后结束\n"
+        "\t为了在测试程序运行结束时立即终止仿真，需要对您的CPU做出如下的改动:\n"
+        "\t\t在顶层添加一个额外的 1bit 输出并命名为 halt\n"
+        "\t\t令当且仅当 WB 段的指令为 0x1000ffff 时其输出有效\n"
+        "\t\t且这个端口必须位于所有的其他输出端口的下方\n"
+        "\t则当 Logisim 发现名为 halt 的端口有效时将终止仿真\n"
+    );
 }
 namespace TXUtility{
     XMLElement* findContent(XMLElement* entry){
@@ -373,13 +425,13 @@ void functionalThread(
         your_ans.open(out_path);
         ret = AnswerComparator::compare(your_ans, ans, comment);
         if(ret == AnswerComparator::PASS){
-            printf("%s通过%s:\t%s\n", test_case.filename().c_str());
+            printf("%s通过%s:\t%s\n", GREENCOLOR, ENDCOLOR, test_case.filename().c_str());
             count_info.pass();
         }else if(ret == AnswerComparator::FAIL){
-            printf("%s失败%s:\t%s: %s\n", test_case.filename().c_str(), comment.c_str());
+            printf("%s失败%s:\t%s: %s\n", REDCOLOR, ENDCOLOR, test_case.filename().c_str(), comment.c_str());
             count_info.fail();
         }else if(ret == AnswerComparator::EXCEPTION){
-            printf("%s异常%s:\t%s: %s\n", test_case.filename().c_str(), comment.c_str());
+            printf("%s异常%s:\t%s: %s\n", PURPLECOLOR, ENDCOLOR, test_case.filename().c_str(), comment.c_str());
             count_info.exception();
         }
     }
@@ -388,6 +440,7 @@ void functionalThread(
 int main(int argc, char** argv){
     Configuration config;
     if(getConfiguration(argc, argv, config) != ERROR_OK){
+        printf("错误: 未识别的选项\n\n");
         showHelp();
         return ERROR_INVALID_OPTION;
     }
@@ -396,6 +449,15 @@ int main(int argc, char** argv){
         return ERROR_OK;
     }
     if(verifyConfiguration(config) != ERROR_OK) return ERROR_INVALID_OPTION;
+    printf(
+        "您的设计位于: %s\n测试程序目录位于: %s\n用于调用Mars的命令: %s\n用于调用Logisim的命令: %s\n使用的线程数: %d\n仿真超时为(秒): %d\n",
+        config.circuit_.c_str(),
+        config.test_dir_.c_str(),
+        config.mars_.c_str(),
+        config.logisim_.c_str(),
+        config.thread_limit_,
+        config.timeout_
+    );
     vector<thread> threads;
     FileAssigner file_assigner(config.test_dir_.c_str());
     CountInfo count_info;
@@ -405,5 +467,6 @@ int main(int argc, char** argv){
         threads.push_back(thread(functionalThread, ref(file_assigner), ref(config), ref(count_info), nonce_base + i));
     functionalThread(file_assigner, config, count_info, nonce_base + config.thread_limit_);
     for(auto& t: threads) t.join();
+    printf("您通过了%lf%%的测试点。\n", count_info.getScore());
     return 0;
 }
